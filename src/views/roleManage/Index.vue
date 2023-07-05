09dabac65282ae0186687f6b15effd196618857e
rayyangyang <2515762599@qq.com>
2023-07-05 10:17:47 +0800

    init

--- src/views/roleManage/Index.vue
+++ src/views/roleManage/Index.vue
@@ -37,10 +37,40 @@
       <div>
         <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
           <el-tab-pane label="web端" name="first">
-            <el-checkbox v-model="isCheckAllWeb" label="全选" size="large" />
+            <el-checkbox v-model="isCheckAllWeb" label="全选" size="large" @change="checkAllWebRole" />
+            <div v-for="(ele, i) in webRoleList">
+              <el-checkbox
+                v-model="ele.checkAll"
+                :indeterminate="ele.isIndeterminate"
+                @change="(e) => handleCheckAllChange(e, ele, i)"
+                >{{ ele.title }}</el-checkbox
+              >
+              <el-checkbox-group
+                v-model="ele.checkChildList"
+                @change="(e) => handleCheckedList(e, ele)"
+                style="margin-left: 20px; margin-bottom: 20px"
+              >
+                <el-checkbox v-for="list in ele.list" :key="list" :label="list" />
+              </el-checkbox-group>
+            </div>
           </el-tab-pane>
           <el-tab-pane label="小程序端" name="second">
-            <el-checkbox v-model="isCheckAllMini" label="全选" size="large" />
+            <el-checkbox v-model="isCheckAllMini" label="全选" size="large" @change="checkAllWxRole" />
+            <div v-for="(ele, i) in wxRoleList">
+              <el-checkbox
+                v-model="ele.checkAll"
+                :indeterminate="ele.isIndeterminate"
+                @change="(e) => handleCheckAllChange(e, ele, i)"
+                >{{ ele.title }}</el-checkbox
+              >
+              <el-checkbox-group
+                v-model="ele.checkChildList"
+                @change="(e) => handleCheckedList(e, ele)"
+                style="margin-left: 20px; margin-bottom: 20px"
+              >
+                <el-checkbox v-for="list in ele.list" :key="list" :label="list" />
+              </el-checkbox-group>
+            </div>
           </el-tab-pane>
         </el-tabs>
       </div>
@@ -79,9 +109,129 @@ onMounted(async () => {
 })
 
 const activeName = ref("first")
+const handleClick = () => {}
+const handleClose = () => {}
 
 // web端
 const isCheckAllWeb = ref(false)
+const checked1 = ref(false)
+
+// role list
+const webRoleList = ref([
+  {
+    title: "角色管理",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["新增角色", "删除角色", "编辑角色", "查看角色"]
+  },
+  {
+    title: "人员管理",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["新增人员", "删除人员", "编辑人员", "查看人员", "重置密码"]
+  },
+  {
+    title: "项目目录",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["新增目录", "删除目录", "编辑目录", "查看详情"]
+  },
+  {
+    title: "农民工工资",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["上传工资", "删除工资", "编辑工资", "查看详情"]
+  },
+  {
+    title: "人员档案",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["上传档案", "删除档案", "编辑档案", "查看详情", "导入档案", "上传体检"]
+  },
+  {
+    title: "项目检查",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["编辑检查", "查看详情"]
+  },
+  {
+    title: "打卡",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["编辑打卡", "查看详情"]
+  }
+])
+
+const wxRoleList = ref([
+  {
+    title: "项目目录",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["查看目录"]
+  },
+  {
+    title: "农民工工资",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["上传工资", "删除工资", "修改工资", "查看工资"]
+  },
+  {
+    title: "人员档案",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["查看详情"]
+  },
+  {
+    title: "项目检查",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["上传项目", "审核检查", "编辑检查", "查看详情"]
+  },
+  {
+    title: "打卡",
+    checkAll: false,
+    isIndeterminate: false,
+    checkChildList: [],
+    list: ["上传打卡", "编辑打卡", "查看详情"]
+  }
+])
+
+const checkAllWebRole = (val) => {
+  for (const prop of webRoleList.value) {
+    prop.checkAll = val
+    prop.checkChildList = val ? prop.list : []
+  }
+}
+
+const checkAllWxRole = (val) => {
+  for (const prop of wxRoleList.value) {
+    prop.checkAll = val
+    prop.checkChildList = val ? prop.list : []
+  }
+}
+
+const handleCheckAllChange = (e, ele, i) => {
+  console.log(123000, ele)
+  ele.checkChildList = e ? ele.list : []
+  ele.isIndeterminate = false
+}
+const handleCheckedList = (e, ele) => {
+  console.log(123)
+  const checkedCount = e.length
+  ele.checkAll = checkedCount === ele.list.length
+  ele.isIndeterminate = checkedCount > 0 && checkedCount < ele.list.length
+}
 
 // 小程序端
 const isCheckAllMini = ref(false)
